b7a9e5c579ec46ccf88ede4f97af76f93de5db67
dragontiger95 <yongbum9508@gmail.com>
2023-03-14 01:05:07 +0900

    브루트 포스 - 블랙잭 (2023.03.13)

---
+++ baekjoon/solved/브루트 포스/2798.py
@@ -0,0 +1,18 @@
+import sys
+
+_max = -999999
+n, m = map(int, input().split())
+card = list(map(int, sys.stdin.readline().split()))
+
+def black_jack(depth, sum, start):
+    global _max
+    if depth > 3 or sum > m:    return
+    elif depth == 3:
+        if _max < sum:  _max = sum
+    else:
+        for i in range(start, len(card)):
+            black_jack(depth+1, sum + card[i], i+1)
+
+black_jack(0,0,0)
+
+print(_max)

be28a14030628946283e463dc17f6cf20ea1a14c
dragontiger95 <yongbum9508@gmail.com>
2023-03-16 23:21:06 +0900

    baekjoon - 브루트포스 - 덩치 (2023.03.16)

---
+++ baekjoon/solved/브루트 포스/7568.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(input())
+prior = [1] * n
+w, h = [0] * n, [0] * n
+
+for i in range(n):
+    _w, _h = map(int, sys.stdin.readline().split())
+    w[i] = _w
+    h[i] = _h
+
+for i in range(n):
+    for j in range(n):
+        if i == j: continue
+        if w[i] < w[j] and h[i] < h[j]:
+            prior[i] += 1
+for i in range(n):
+    print(prior[i], end=' ')
+print()
